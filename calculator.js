d2115ce6733e083aa6295ac8f2009ebfa78991b8
tonipek <tonipek@metropolia.fi>
2023-03-28 16:09:43 +0300

    123

--- calculator.js
+++ calculator.js
@@ -2,7 +2,7 @@ const add = (numbers) => {
     return numbers
         .split(',')
         .map(x => parseInt(x))
-        .reduce((a, b) => a + b + 1);
+        .reduce((a, b) => a + b);
 };
  
 exports.add = add;
--- calculator.test.js
+++ calculator.test.js
@@ -3,7 +3,7 @@ const calculator = require('./calculator')
 test('string with a single number should result in the number itself', () => {
     expect(calculator.add('1')).toBe(1);
   });
-/*
+
 test('string with two numbers separated by comma should result in the sum of the numbers', () => {
   expect(calculator.add('4, 5')).toBe(9);
 });
@@ -15,4 +15,3 @@ test('string with three numbers separated by comma should result in the sum of t
 test('string with four numbers separated by comma should result in the sum of the numbers', () => {
   expect(calculator.add('2, 0, 4, 5')).toBe(11);
 });
-*/

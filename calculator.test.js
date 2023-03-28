765f7cb38fdd8be6776ec0e6425b9308e3713806
tonipek <tonipek@metropolia.fi>
2023-03-28 14:55:52 +0300

    test

--- calculator.test.js
+++ calculator.test.js
@@ -1,17 +1,17 @@
-import {add} from './calculator';
+const calculator = require('./calculator');
 
 test('string with a single number should result in the number itself', () => {
-  expect(add('1')).toBe(1);
+  expect(calculator.add('1')).toBe(1);
 });
 
 test('string with two numbers separated by comma should result in the sum of the numbers', () => {
-  expect(add('4, 5')).toBe(9);
+  expect(calculator.add('4, 5')).toBe(9);
 });
 
 test('string with three numbers separated by comma should result in the sum of the numbers', () => {
-  expect(add('2, 8, 4')).toBe(14);
+  expect(calculator.add('2, 8, 4')).toBe(14);
 });
 
 test('string with four numbers separated by comma should result in the sum of the numbers', () => {
-  expect(add('2, 0, 4, 5')).toBe(11);
+  expect(calculator.add('2, 0, 4, 5')).toBe(11);
 });
